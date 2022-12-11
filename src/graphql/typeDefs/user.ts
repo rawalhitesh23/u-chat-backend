0c4feed32d6df331fab91162c2edbb8e5d3bd0ae
hitesh <rawalhitesh3520@gmail.com>
2022-12-11 09:41:02 +0530

    Initial commit | graphql configuration done

---
+++ src/graphql/typeDefs/user.ts
@@ -0,0 +1,21 @@
+const typeDefs = `
+    type User {
+        id: String
+        username: String
+    }
+
+    type Query {
+        searchUsers(username: String): [User]
+    }
+
+    type Mutation {
+        createUsername(username: String): CreateUsernameResponse 
+    }
+
+    type CreateUsernameResponse {
+        success: Boolean
+        error: String
+    }
+`;
+
+export default typeDefs;
